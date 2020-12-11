0d9221c634fb35ed8b7d6f4c66874216100bc5f1
yuuki itoi winodws <yuuki.itoi6120@gmail.com>
2020-12-12 03:30:35 +0900

    test用のコントローラーを空白に

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,16 +1,5 @@
+require 'line/bot'
+
 class TestsController < ApplicationController
-  def index
-    # 返信したいメッセージ
-    message = {
-      type: 'text',
-      text: 'hello'
-    }
-    # 
-    client = Line::Bot::Client.new { |config|
-        config.channel_secret = "<channel secret>"
-        config.channel_token = "<channel access token>"
-    }
-    response = client.reply_message("<replyToken>", message)
-    p response    
-  end
+
 end
