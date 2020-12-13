90578e04c2848745770dce6b0daa847132069366
yuuki itoi winodws <yuuki.itoi6120@gmail.com>
2020-12-13 23:48:51 +0900

    DB更新

--- db/migrate/20201212120243_add_details_to_line_bots.rb
+++ db/migrate/20201212120243_add_details_to_line_bots.rb
@@ -2,5 +2,6 @@ class AddDetailsToLineBots < ActiveRecord::Migration[6.0]
   def change
     add_column :line_bots, :message_sender, :string
     add_column :line_bots, :received_message, :string
+    add_column :line_bots, :settlement_judge, :string
   end
 end
