955784769a3616d109aab3a9d1969af0b6fcbbe7
Yuuki Itoi <yuuki.itoi6120@gmail.com>
2021-03-25 22:08:44 +0900

    管理画面の元作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tests', to: 'tests#index'
   post 'line_bots', to: 'line_bots#callback'
+  get 'admin', to: 'admins#index'
 end
