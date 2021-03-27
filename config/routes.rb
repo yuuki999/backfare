8005b542435ad223a3f7affce410c5bdacf920f7
Yuuki Itoi <yuuki.itoi6120@gmail.com>
2021-03-27 20:21:05 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin_users
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tests', to: 'tests#index'
   post 'line_bots', to: 'line_bots#callback'
   get 'admin', to: 'admins#index'
+
+  # どのパスにも一致しない場合。
+  # TODO: 404 not found ページを用意する。
+  match '*path', to: redirect('/'), via: :all
 end
