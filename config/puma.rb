ed9ea8fcbd8ea0e85f9a7912056ca4e3ed3a444e
Yuuki Itoi <yuuki.itoi6120@gmail.com>
2021-04-11 17:31:40 +0900

    nginx導入

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,8 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# ソケット通信する場合はコメントアウト
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -36,3 +37,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+app_root = File.expand_path("../../", __FILE__) # ./config/pumaで起動する分の階層上げ。
+# nginx + puma bind
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
